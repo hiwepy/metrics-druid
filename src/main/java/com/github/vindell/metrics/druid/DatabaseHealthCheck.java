7e4a28c3a640641293174bfec012f06f343b2ccc
vindell <hnxyhcwdl1003@163.com>
2017-06-29 21:00:46 +0800

    提交代码

---
+++ src/main/java/com/github/vindell/metrics/druid/DatabaseHealthCheck.java
@@ -0,0 +1,31 @@
+package com.github.vindell.metrics.druid;
+
+import com.alibaba.druid.stat.DruidDataSourceStatManager;
+import com.alibaba.druid.stat.DruidStatService;
+import com.codahale.metrics.health.HealthCheck;
+
+public class DatabaseHealthCheck extends HealthCheck {
+
+	private DruidStatService statService = DruidStatService.getInstance();
+
+	public DatabaseHealthCheck() {
+	}
+
+	@Override
+    public HealthCheck.Result check() throws Exception {
+    	
+		statService.getWallStatMap(null);
+		
+		DruidDataSourceStatManager.getInstances();
+		
+		
+		DruidStatService.registerMBean();
+    	
+    	
+        if (database.isConnected()) {
+            return HealthCheck.Result.healthy();
+        } else {
+            return HealthCheck.Result.unhealthy("Cannot connect to " + database.getUrl());
+        }
+    }
+}
